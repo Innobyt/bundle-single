31122d7078dde6b1a3a79eca8f4013dbc4d9fe7f
Kyle Haithir <kyle@innobyt.com>
2014-11-07 16:31:47 +0800

    add/change new gamerepoth collection remove/change property threshold

--- server/api/game-repo/gamerepo.model.js
+++ server/api/game-repo/gamerepo.model.js
@@ -4,7 +4,6 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var GamerepoSchema = new Schema({
-    threshold   : Number,
     gamename    : String,
     gamekey     : String,
     keystatus	: Boolean
