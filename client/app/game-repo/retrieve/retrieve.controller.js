7788944c31f77b5fea4dbb91a4b80ce989a4dfbc
Innobyt <dev@innobyt.com>
2014-11-30 19:51:04 -0700

    Update front-end filtering gamerepo

--- client/app/game-repo/retrieve/retrieve.controller.js
+++ client/app/game-repo/retrieve/retrieve.controller.js
@@ -1,8 +1,8 @@
 (function() {
 	'use strict';
 
-	angular.module('gameRepoApp')
-	  .controller('RetrieveCtrl', ['$scope', 'gameRepo', RetrieveCtrl]);
+	angular.module('gameRepoApp', ['ngTable'])
+	  .controller('RetrieveCtrl', ['$scope', $filter, ngTableParams, 'gameRepo', RetrieveCtrl]);
 
 	// RetrieveCtrl requires 1 actions of CRUD, R as in retrieve
 	function RetrieveCtrl($scope, gameRepo) {
@@ -13,11 +13,33 @@
 				$scope.gameRepo = gameRepo;
 			});
 		};
+		
+		var data = $scope.readall();	
+		
+		$scope.tableParams = new ngTableParams({
+        page: 1,            // show first page
+        count: 10,          // count per page
+        filter: {
+            gamename: 'game1234'       // initial filter
+        }
+    }, {
+        total: data.length, // length of data
+        getData: function($defer, params) {
+            // use build-in angular filter
+            var orderedData = params.filter() ?
+                   $filter('filter')(data, params.filter()) :
+                   data;
 
+            $scope.gamerepos = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
+
+            params.total(orderedData.length); // set total for recalc pagination
+            $defer.resolve($scope.gamerepos);
+        }
+    });
+		
 		// initialize gameRepo controller and services
 		$scope.initialize = function(){
 			$scope.readall();
-			$scope.gameRepo = gamequery();
 		};
 
 		$scope.initialize();
