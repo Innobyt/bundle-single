aca04c6acba08dac8010f53b78ef61e181d69455
Innobyt <dev@innobyt.com>
2014-12-01 22:31:44 -0700

    Added CSV Parser (create part) & Ng-Table For Available/Claimed Keys + Export (update part)

--- client/app/game-repo/create/create.controller.js
+++ client/app/game-repo/create/create.controller.js
@@ -16,6 +16,26 @@
 		$scope.submit = function() {
 			$scope.formData.$save(function(){ $scope.initialize(); });
 		 };
+		
+		//Not sure how to implement the watch.
+		/*$scope.$watch("gamekeys", function() {
+			var lines, lineNumber, data, length;
+			$scope.gamekeysList = [];
+			lines = $scope.gamekeys.split('\n');
+			lineNumber = 0;
+				for (var i = lines.length - 1; i >= 0; i--) {
+				l = lines[i];
+
+				lineNumber++;
+				data = l.split(',');
+
+				var gamekeysName = data[0];
+
+				$scope.gamekeysList.push({
+					gamekeys: gamekeysName
+				});
+			}
+		});*/
 
 		$scope.initialize();
 	};
