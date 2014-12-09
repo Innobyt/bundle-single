0ad026f081ec537bfd9fa7bbc1fe4c860a44295c
Innobyt <dev@innobyt.com>
2014-12-08 22:35:51 -0700

    Still solving in displaying unique records from list and ng-table with custom angularjs filter

--- client/app/game-repo/retrieve/retrieve.controller.js
+++ client/app/game-repo/retrieve/retrieve.controller.js
@@ -1,5 +1,7 @@
 (function() {
 	'use strict';
+	
+	var Gametitles = require('../../../server/api/game-repo/gametitle.model');
 
 	// Mon, December 1, 2014 fix, comments (you can delete these comments): 
 	// I have commented out, various code in order to get this module working properly.
@@ -7,20 +9,40 @@
 
 	angular.module('gamerepoApp')
 	  .controller('RetrieveCtrl', ['$scope', $filter, ngTableParams, 'gameRepo', RetrieveCtrl]);
+	
+	// Building Custom Filter - Reference: http://stackoverflow.com/questions/18381944/ng-options-and-unique-filter-not-displaying-angular-js
+	// and http://stackoverflow.com/questions/24832192/angularjs-how-to-get-the-unique-values-in-ng-option
+	angular.module('gamerepoApp', [])
+	   .filter('unique', function($parse) {
+		  return function(input, filter) {
+			if(angular.isArray(input)) {
 
+			  //unique key
+			  var getter = $parse(filter);
+
+			  return _.uniq(input, function(elm) {
+				  return getter(elm);
+			  });
+			}   
+		   return input;
+		  }
+		});
+	
 	// RetrieveCtrl requires 1 actions of CRUD, R as in retrieve
 	function RetrieveCtrl($scope, gameRepo) {
 
 		console.log('now this is loaded');
 		
-		var data = $scope.gameRepo;	
+		var data = $scope.readall();
+		
+		console.log('Gametitles Data' + data);
 		
 		$scope.tableParams = new ngTableParams({
 			page: 1,            // show first page
 			count: 10,          // count per page
 			filter: {
-						gamename: 'game1234',       // initial filter for gamename
-						gamekeys: '', // search for gamekeys
+						gamename: 'game1234'       // initial filter for gamename
+						gamekeys: '' // search for gamekeys
 						keystatus: '' // Has to implement as "Available" and "Claimed", not sure how to create the status as this.
 					}
 					
@@ -46,9 +68,11 @@
 			
 		// get, all gameRepo ('R' in Crud)
 		$scope.readall = function(){
-			var gamerepo = gameRepo.query(function() {
-				$scope.gamerepo = gamerepo;
-			});
+			var gamerepo = Gametitles.find(function(err, gametitles)){
+			if(err){ return next(err); }
+			res.json(gametitles);
+			console.log('Gamerepos Record' + gamerepo);
+		});
 		};
 		
 		// initialize gameRepo controller and services
